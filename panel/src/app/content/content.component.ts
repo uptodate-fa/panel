171ec964d3245260e07608943b39b76a1cb05de7
lida Gholamian <ta.gholamian@gmail.com>
2024-09-01 11:09:27 +0330

    styles add

--- panel/src/app/content/content.component.ts
+++ panel/src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, computed, signal, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Content } from '@uptodate/types';
@@ -14,13 +14,38 @@ import { SHARED } from '../shared';
   styleUrl: './content.component.scss',
 })
 export class ContentComponent {
+  @ViewChild('contentHTML') contentHTML : any;
   content = signal<Content | null>(null);
+  bodyHTML = computed (() => {
+    const body = this.content()?.bodyHtml;
+    if (body){
+      const div = document.createElement('div');
+      div.innerHTML = body;
+
+      const allInnerDivs = div.querySelectorAll('div');
+      console.log(allInnerDivs);
+
+      allInnerDivs.forEach(element => {
+        if( element.id) {
+          element.classList.add(element.id)
+        }
+      });
+
+      return (div.innerHTML)
+      
+    }
+
+  
+    return body;
+    
+  })
   constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.route.params.subscribe((params) => {
       const id = params['id'];
       if (id) this.load(id);
     });
   }
+  
 
   async load(id: string) {
     this.content.set(null);
