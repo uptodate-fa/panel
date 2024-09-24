516c075f278687fb2cb04cc68f800e3f4d90de84
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-09-24 16:22:50 +0330

    content outline and enamad

---
+++ panel/src/app/search/result/search-result-card/search-result-card.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject, Input, signal } from '@angular/core';
+import { SHARED } from '../../../shared';
+import { MatCardModule } from '@angular/material/card';
+import { Content, SearchResult } from '@uptodate/types';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-result-card',
+  standalone: true,
+  imports: [CommonModule, SHARED, MatCardModule],
+  templateUrl: './search-result-card.component.html',
+  styleUrl: './search-result-card.component.scss',
+})
+export class SearchResultCardComponent {
+  private http = inject(HttpClient);
+
+  @Input({required: true}) result: SearchResult;
+  isMouseOver = signal(false);
+  
+  outlineQuery = injectQuery(() => ({
+    queryKey: ['outline', this.result.id],
+    queryFn: () =>
+      lastValueFrom(
+        this.http.get<Content>(
+          `/api/contents/outline/${this.result.id}`
+        )
+      ),
+    enabled: this.isMouseOver(),
+    staleTime: Infinity,
+  }));
+
+  onMouseOver(ev: MouseEvent) {
+    console.log(ev);
+  }
+}
