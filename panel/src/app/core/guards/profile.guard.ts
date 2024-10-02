51fa69608fc30eb9f1639687daa8290f87ddc932
Mahdi Ketabdar <mahdi7192@gmail.com>
2024-10-02 10:46:12 +0330

    profile complete

---
+++ panel/src/app/core/guards/profile.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ProfileDialogComponent } from '../../shared/dialogs/profile-dialog/profile-dialog.component';
+
+export const profileGuard: CanActivateFn = async () => {
+  const auth = inject(AuthService);
+  const dialog = inject(MatDialog);
+  setTimeout(() => {
+    if (!auth.isProfileComplete) {
+      dialog.open(ProfileDialogComponent, {
+        disableClose: true,
+      });
+    }
+  }, 3000);
+  return true;
+};
--- panel/src/app/shell/shell.component.ts
+++ panel/src/app/shell/shell.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
+import { ProfileDialogComponent } from '../shared/dialogs/profile-dialog/profile-dialog.component';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-shell',
@@ -20,4 +23,11 @@ import { RouterModule } from '@angular/router';
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss',
 })
-export class ShellComponent {}
+export class ShellComponent {
+  private dialog = inject(MatDialog);
+  auth = inject(AuthService);
+
+  openProfile() {
+    this.dialog.open(ProfileDialogComponent);
+  }
+}
