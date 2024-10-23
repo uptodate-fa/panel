49d3dd76771d59e1a3566785196440c13dd7b5e1
lida Gholamian <ta.gholamian@gmail.com>
2024-10-23 10:04:29 +0330

    feat(drug interactions): some style change

--- panel/src/app/drug-interactions/result/res-table/res-table.component.ts
+++ panel/src/app/drug-interactions/result/res-table/res-table.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-res-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule],
   templateUrl: './res-table.component.html',
   styleUrl: './res-table.component.scss',
 })
