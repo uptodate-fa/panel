895f31a798cb043d3c3b15958c5043537ac4fc0e
Mahdi Ketabdar <mahdi7192@gmail.com>
2025-01-26 14:09:16 +0330

    fix: add index to queryStringId in ContentSchema for improved query performance

--- backend/src/app/schemas/content.schema.ts
+++ backend/src/app/schemas/content.schema.ts
@@ -4,7 +4,7 @@ import mongoose, { Types } from 'mongoose';
 export const ContentSchema = new mongoose.Schema<Content>(
   {
     uptodateId: { type: String, required: true },
-    queryStringId: { type: String, required: true },
+    queryStringId: { type: String, required: true, index: true },
     title: { type: String, required: true },
     bodyHtml: { type: String, required: true },
     outlineHtml: { type: String, required: true },
