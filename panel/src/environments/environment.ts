e579906c2a59801f40403bb615a9ad62d783a88e
Mahdi Ketabdar <mahdi7192@gmail.com>
2025-03-13 23:53:03 +0330

    feat: update styling and layout; enhance dialog responsiveness and add logout button to menu

--- panel/src/environments/environment.ts
+++ panel/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://uptodate-api.darkube.app',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://uptodate-api.darkube.app',
+  // apiUrl: 'http://localhost:3000',
 };
 
 /*
